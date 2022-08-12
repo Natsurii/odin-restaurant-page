37503591c3a9a9fe067aabefdcd07328dfe9ae78
Natsurii <unatsurii@gmail.com>
2022-08-12 16:01:17 +0800

    update cpnfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
-const { resolve } = require('path');
 const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry : path.resolve(__dirname, 'src/index.js'),
-    output : {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|svg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ],
 
+    }
 }
