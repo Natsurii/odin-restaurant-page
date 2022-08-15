875be03d419bb8d2f3ef4041604be0d37bc3b03d
Natsurii <unatsurii@gmail.com>
2022-08-15 14:48:30 +0800

    add remove childs function

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,12 @@
 
 const createDiv = () => document.createElement('div');
+const removeChilds = (parent) => {
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
+    }
+};
 
 export {
-    createDiv
+    createDiv,
+    removeChilds
 }
